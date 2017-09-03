72fd31d362fc7f2239cc652e1439a09a15784433
carlosalmonte04 <vacarlos_alm@hotmail.com>
2017-09-03 14:01:04 -0400

    'seeds'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Post.create!([
+  {title: "Self Taught vs. Coding Bootcamp", content: nil, link: "https://medium.com/@carlosalmonte04/self-taught-vs-coding-bootcamp-d8073bd10f37?source=rss-3702aac98c50------2", img: "post1.jpg"},
+  {title: "TDD, why use it?", content: "", link: "https://medium.com/@carlosalmonte04/tdd-why-use-it-7747e6e091c4?source=rss-3702aac98c50------2", img: "post2.png"},
+  {title: "Bleach The Views With AJAX", content: nil, link: "https://medium.com/@carlosalmonte04/bleach-the-views-with-ajax-737443c6dc88?source=rss-3702aac98c50------2", img: "post3.jpg"},
+  {title: "React on Plain Javascript Terms", content: nil, link: "https://medium.com/@carlosalmonte04/react-on-plain-javascript-terms-7521d00f0d99?source=rss-3702aac98c50------2", img: "post4.jpg"}
+])
